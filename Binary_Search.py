3ae823adc855121de02642baee9cc22425bfafae
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2019-10-13 20:41:52 +0300

    Добавлена проверка типов входных данных

--- Binary_Search.py
+++ Binary_Search.py
@@ -12,6 +12,11 @@ class BinarySearch:
     def Step(self,N):
         #выполняет один шаг поиска: делит текущий диапазон на два, продолжает сокращение рабочего диапазона,
         #корректируя Left и Right, при необходимости фиксирует завершение поиска
+        for i in range(0,self.array_len):
+            if type(self.array[i])!= int:
+                return -1
+        if self.array_len==0:
+            return None
         middle_index=(self.left+self.right+1)//2
         middle_value=self.array[middle_index]
         if N>middle_value:
@@ -26,6 +31,11 @@ class BinarySearch:
             pass 
 
     def GetResult(self):
+        for i in range(0,self.array_len):
+            if type(self.array[i])!= int:
+                return -1
+        if self.array_len==0:
+            return -1
         if self.Success==True:
             return 1
         else:
@@ -33,15 +43,16 @@ class BinarySearch:
                 return 0
             elif self.Stop_Search==True:
                 return -1
+
 """
-arr=[6,7,8,9,14,32,34,56]
+
+arr=[1,23,"e","e"]
 A=BinarySearch(arr)
-for _ in range(0,5):
-    A.Step(10)
+for _ in range(0,4):
+    A.Step("e")
     print(A.left,A.right)
     print(A.GetResult())
 """
-
 
 
 
