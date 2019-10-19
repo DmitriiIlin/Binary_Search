9487a0b6b4d177fd03647266b9e9456b40b65ba2
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2019-10-19 14:45:56 +0300

    Добавлена ф-ция GallopingSearch

--- Binary_Search.py
+++ Binary_Search.py
@@ -12,6 +12,8 @@ class BinarySearch:
     def Step(self,N):
         #выполняет один шаг поиска: делит текущий диапазон на два, продолжает сокращение рабочего диапазона,
         #корректируя Left и Right, при необходимости фиксирует завершение поиска
+        last_right=self.Right
+        last_left=self.Left
         for i in range(0,self.array_len):
             if type(self.array[i])!= int:
                 return -1
@@ -25,7 +27,7 @@ class BinarySearch:
             self.Right=middle_index
         else:
             self.Success=True
-        if self.Left==self.Right:
+        if self.Left==self.Right or (last_left==self.Left and last_right==self.Right):
             self.Stop_Search=True
         else:
             pass 
@@ -44,15 +46,56 @@ class BinarySearch:
             elif self.Stop_Search==True:
                 return -1
 
-"""
 
-arr=[1,23,"e","e"]
-A=BinarySearch(arr)
-for _ in range(0,4):
-    A.Step("e")
-    print(A.left,A.right)
-    print(A.GetResult())
+#12 Занятие
+
+def GallopingSearch(array,N):
+    #Блок 1 : Поиск диапазона нахождения искомого элемента
+    i=1
+    max_index=len(array)-1
+    right=len(array)-1
+    left=0
+    index=2**i-2
+    while True:
+        if array[index]==N:
+            return True
+        elif array[index]<N:
+            i+=1
+            index=2**i-2
+            if index>max_index:
+                index=max_index
+                right=max_index
+                if array[index]==N:
+                    return True
+                else:
+                    return False
+            else:
+                pass
+        else:
+            right=index
+            left=2**(i-1)-1
+            break
+    #Блок 2 : Создание массива и поиск позиции с помошью метода Step класса BinarySearch 
+    res_arr=[]
+    for j in range(left,right+1):
+        res_arr.append(array[j])
+    Output=BinarySearch(res_arr)
+    while Output.Success!=True:
+        Output.Step(N)
+        if Output.Stop_Search==True and Output.Success!=True:
+            return False
+        else:
+            pass
+    return True
+
+
+
 """
+arr=[1,23,90,97,101,104,567]
+print(GallopingSearch(arr,7))
+"""    
+    
+
 
 
 
